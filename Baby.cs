8ea50c55b76a04f9181af8cf7859a9664646d61c
Mert Topcu <merttopcu.dev@gmail.com>
2024-08-02 11:17:53 +0300

    Add project files.

---
+++ Baby.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatikaConstructorTask
+{
+    public class Baby
+    {
+        public DateTime birthDate { get; set; }
+        public string name { get; set; }
+        public string surName { get; set; }
+
+
+        public Baby()
+        {
+            Console.WriteLine("Ingaaaa");
+            this.birthDate = DateTime.Now;
+        }
+        public Baby(string name, string surName)
+        {
+            this.name = name;
+            this.surName = surName;
+            Console.WriteLine("Ingaaaa");
+            this.birthDate = DateTime.Now;
+        }
+
+        public void Greeting()
+        {
+            Console.WriteLine($"Name: {this.name}\nSurname: {this.surName}\nBirthdate: {this.birthDate.ToString("dd.MM.yyyy")}\nCongratulations! You have baby!");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatikaConstructorTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // ▼ Creating instance of baby object with 'parameters' constructor 2. ▼
+            Baby babyMert=new Baby("Mert","Topcu");
+            // ▼ Printing informations of baby ▼
+            babyMert.Greeting();
+
+            // ▼ Creating instance of baby object with default constructor 1. ▼
+            Baby babyDefault = new Baby();
+            babyDefault.name = "Ahmet";
+            babyDefault.surName = "Mehmet";
+            // ▼ Printing informations of baby ▼
+            babyDefault.Greeting();
+        }
+    }
+}
